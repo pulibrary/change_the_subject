5975744c0324adbca0c3bc8be704da0a4b5f026a
Max Kadel <mkadel@princeton.edu>
2022-09-14 14:37:48 -0400

    Generated the repository

---
+++ lib/change_the_subject.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "change_the_subject/version"
+
+module ChangeTheSubject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/change_the_subject/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ChangeTheSubject
+  VERSION = "0.1.0"
+end
