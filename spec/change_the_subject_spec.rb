d62f61697a954ff17c9d3f98641702b86f9b0fa5
Max Kadel <mkadel@princeton.edu>
2022-09-15 10:42:48 -0400

    Limit mocked config context to test that needs it

--- spec/change_the_subject_spec.rb
+++ spec/change_the_subject_spec.rb
@@ -32,16 +32,6 @@ RSpec.describe ChangeTheSubject do
       expect(described_class.check_for_replacement(term: "Indians of Central America")).to eq("Indigenous peoples of Central America")
       expect(described_class.check_for_replacement(term: "Indians of North America")).to eq("Indigenous peoples of North America")
     end
-  end
-
-  context "with a mocked configuration" do
-    around do |example|
-      described_class.remove_instance_variable(:@terms_mapping) if described_class.instance_variables.include?(:@terms_mapping)
-      described_class.remove_instance_variable(:@config) if described_class.instance_variables.include?(:@config)
-      example.run
-      described_class.remove_instance_variable(:@terms_mapping) if described_class.instance_variables.include?(:@terms_mapping)
-      described_class.remove_instance_variable(:@config) if described_class.instance_variables.include?(:@config)
-    end
 
     context "a term that has not been replaced" do
       let(:subject_term) { "Daffodils" }
@@ -87,23 +77,34 @@ RSpec.describe ChangeTheSubject do
       end
     end
 
-    context "subject terms that have empty replacements" do
-      let(:subject_terms) { ["Test term", "Illegal aliens"] }
-      let(:fixed_subject_terms) { ["Undocumented immigrants"] }
-      let(:fixture_config) { File.join("spec", "fixtures", "change_the_subject.yml") }
-
-      before do
-        allow(File).to receive(:join).and_return(fixture_config)
-      end
+    context "Indigenous studies terms" do
+      let(:subject_terms) { ["Indians of North America—Connecticut"] }
+      let(:fixed_subject_terms) { ["Indigenous peoples of North America—Connecticut"] }
 
       it "suggests a replacement" do
         expect(described_class.fix(subject_terms: subject_terms)).to eq fixed_subject_terms
       end
     end
+  end
 
-    context "Indigenous studies terms" do
-      let(:subject_terms) { ["Indians of North America—Connecticut"] }
-      let(:fixed_subject_terms) { ["Indigenous peoples of North America—Connecticut"] }
+  context "with a mocked configuration" do
+    around do |example|
+      described_class.remove_instance_variable(:@terms_mapping) if described_class.instance_variables.include?(:@terms_mapping)
+      described_class.remove_instance_variable(:@config) if described_class.instance_variables.include?(:@config)
+      example.run
+      described_class.remove_instance_variable(:@terms_mapping) if described_class.instance_variables.include?(:@terms_mapping)
+      described_class.remove_instance_variable(:@config) if described_class.instance_variables.include?(:@config)
+    end
+
+    let(:fixture_config) { File.join("spec", "fixtures", "change_the_subject.yml") }
+
+    before do
+      allow(File).to receive(:join).and_return(fixture_config)
+    end
+
+    context "subject terms that have empty replacements" do
+      let(:subject_terms) { ["Test term", "Illegal aliens"] }
+      let(:fixed_subject_terms) { ["Undocumented immigrants"] }
 
       it "suggests a replacement" do
         expect(described_class.fix(subject_terms: subject_terms)).to eq fixed_subject_terms
